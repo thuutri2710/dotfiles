226b30a2b850231954488dda052288cb2a5366f7
zlatan <zlatan.pham@gmail.com>
2019-04-21 00:09:13 +0700

    update brew.sh

--- brew.sh
+++ brew.sh
@@ -52,49 +52,25 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
+# Bash tools
+brew install tmux
+brew install fzf
 brew install tree
-brew install vbindiff
-brew install zopfli
+brew install node
 
+# Applications
+# brew cask install --appdir="/Applications" your-app
+brew cask install insomnia
+brew cask install iterm2
+brew cask install slack
+brew cask install visual-studio-code
+brew cask install spotify
+brew cask install 1password
+brew cask install firefox
+brew cask install dropbox
+brew cask install skype
+brew cask install karabiner
+brew cask install spectacle
+brew cask install kitty
 # Remove outdated versions from the cellar.
 brew cleanup
